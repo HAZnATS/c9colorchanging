a9d640328b8da8987c836f01b7d902bc2bf19c7d
HAZnATS <87596379+HAZnATS@users.noreply.github.com>
2021-11-12 13:58:44 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw()
+{
+  background(30);
+if(keyIsDown(UP_ARROW)){
+background("blue");
+}
+if(keyIsDown(DOWN_ARROW)){
+  background("red");
+}
+}
+
+
+
+
